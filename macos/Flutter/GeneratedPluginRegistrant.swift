f808b7d88d639b91c1eb19b778048a1967499d4b
Huỳnh Mỹ Tuân <huynhmytuan.dev@gmail.com>
2025-04-01 23:22:02 +0700

    feat(speech_to_text_client): add initial implementation of speech-to-text client
    Introduce the core functionality for speech-to-text recognition, including initialization, language selection, and result handling. This includes the necessary configurations for both Android and iOS platforms, along with test coverage and CI/CD integration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
+import speech_to_text
 import sqflite_darwin
 import url_launcher_macos
 
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
